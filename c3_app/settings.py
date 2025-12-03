c98a9696c541427a0134c8d8d23f059e8e9278a7
integral <tk.07@outlook.jp>
2025-12-04 00:35:07 +0900

    [fix] delete static ref, which out of each app

--- c3_app/settings.py
+++ c3_app/settings.py
@@ -170,9 +170,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',  # 共通staticは使用しない
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
