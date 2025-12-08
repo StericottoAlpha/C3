9c0f4e68519bed8f289b17c36bc24c339b9366a5
takano manaot <tk.manato@outlook.jp>
2025-12-08 11:14:51 +0900

    [add] 追加漏れadminへのモデル登録

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PageVisit
 
-# Register your models here.
+
+@admin.register(PageVisit)
+class PageVisitAdmin(admin.ModelAdmin):
+    """ページ訪問管理画面"""
+
+    list_display = ['id', 'message', 'visited_at']
+    list_filter = ['visited_at']
+    search_fields = ['message']
+    ordering = ['-visited_at']
+
+    readonly_fields = ['visited_at']
