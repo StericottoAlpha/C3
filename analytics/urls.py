3ea725041cdd3c8f207fd61523e9a3079953d991
Daito KATO <t23cs012@yamanashi.ac.jp>
2025-12-19 15:35:33 +0900

    [add]グラフ作成まで

--- analytics/urls.py
+++ analytics/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'analytics'
 
 urlpatterns = [
-    path('graph/own_store/', views.dashboard, name='dashboard'),
+    path('graph/own_store/', views.dashboard, {'default_scope': 'own'}, name='dashboard'),
+    path('graph/all_store/', views.dashboard, {'default_scope': 'all'}, name='dashboard_all'),
     path('api/graph-data/', views.get_graph_data, name='graph_data'),
     path('api/monthly-goal/', views.get_monthly_goal, name='monthly_goal'),
 ]
