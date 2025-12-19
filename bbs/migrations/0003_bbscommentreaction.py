d765c01c51093575323dd243c23d3accd0a6899c
takano manaot <tk.manato@outlook.jp>
2025-12-19 15:54:22 +0900

    [fix] migration fix

--- bbs/migrations/0003_bbscommentreaction.py
+++ bbs/migrations/0003_bbscommentreaction.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bbs', '0001_initial'),
+        ('bbs', '0002_alter_bbspost_genre'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
